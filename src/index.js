a960650f3d7ffd996212fcb139e4acae6111ddf6
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-01-06 03:33:30 +0300

    react-redux-app__FP_currying__2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {compose, pipe} from "lodash/fp";
 
 const App = () => {
-    return <h1>App</h1>
+    const x = 2;
+    const double = (number) => number * 2;
+    const square = (number) => number * number;
+    const half = (number) => number / 2;
+    const divide = (num2) => {
+      return function (num1) {
+          return num1 / num2;
+      }
+    }
+    const mathCalculate = pipe(double, square, half, divide(3));
+    return <h1>{mathCalculate(x)}</h1>
 }
 
 ReactDOM.render(
