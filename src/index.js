2f636007af04132408e0d6c80b45192d79d3e996
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-01-08 23:23:29 +0300

    react-redux-app__ducks_pattern__15

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
-import * as actions from "./store/task/actions";
+import { taskCompleted, titleChanged, taskDeleted } from "./store/task";
 
 const store = configureStore();
 
@@ -13,14 +13,14 @@ const App = () => {
     });
   }, []);
   const completeTask = (taskId) => {
-    store.dispatch(actions.taskCompleted(taskId));
+    store.dispatch(taskCompleted(taskId));
   };
 
   const changeTitle = (taskId) => {
-    store.dispatch(actions.titleChanged(taskId));
+    store.dispatch(titleChanged(taskId));
   };
   const deleteTask = (taskId) => {
-    store.dispatch(actions.taskDeleted(taskId));
+    store.dispatch(taskDeleted(taskId));
   };
 
   return (
